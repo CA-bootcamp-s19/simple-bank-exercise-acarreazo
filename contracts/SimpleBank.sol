266293771a9a5a496116e11de12f22902958ed93
Anllelo Carreazo <anllelocarreazo@gmail.com>
2019-07-31 23:24:05 -0500

    Update SimpleBank.sol
    require(enrolled[msg.sender]==true,"Users should be enrolled before they can make deposits");

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -82,6 +82,7 @@ contract SimpleBank {
     function deposit() public payable returns (uint) {
         /* Add the amount to the user's balance, call the event associated with a deposit,
           then return the balance of the user */
+        require(enrolled[msg.sender]==true,"Users should be enrolled before they can make deposits");
         balances[msg.sender] += msg.value;
         emit LogDepositMade(msg.sender,msg.value);
         return balances[msg.sender];
